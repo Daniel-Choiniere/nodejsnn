1a1e3d9d4ac83380ba28bb9741f2b304e2990e3b
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-04 01:13:23 +0000

    different function types

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-// var time = 0;
-// console.log(__dirname);
-// console.log(__filename);
-
-// setInterval(function(){
-//     time += 2;
-//     console.log(time + ' seconds have passed');
-// }, 2000);
+// two types of functions
+function sayHi() {
+    console.log('hi');
+}
 
 
-
-
-
-
-
-
-// var fs = require('fs');
-
-// var file = fs.readFile('input.txt','utf8',function(err,data){
-//     fs.writeFile('writeme.txt',data);
-//     console.log(data);
-// });
-
-// console.log('This is an instruction outside the sync file system.');
+var sayBye = function() {
+    console.log('bye');
+}
